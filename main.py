2b6c72408ee394beff7d3eb0489189367bf07d95
Mimi <emilieseneclauze09@gmail.com>
2025-04-27 00:20:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Je suis en vie !"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
 import discord
 from discord.ext import commands
 import json

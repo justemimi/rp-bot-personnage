497a5c77207461dc0896fca26d33e2ba7f1d1455
Mimi <emilieseneclauze09@gmail.com>
2025-05-27 21:07:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,22 @@ import threading
 import http.server
 import socketserver
 
-# Choisis ton port
-PORT = 8080
+# keep_alive.py
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Je suis vivant !"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
 
 # Handler pour répondre à toutes les requêtes entrantes
 class Handler(http.server.SimpleHTTPRequestHandler):
